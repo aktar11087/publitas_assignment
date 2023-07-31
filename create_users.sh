a1cf61d8dc728b81be596718f8b5188f4e890d3c
Aktar Hossain <83822984+aktar11087@users.noreply.github.com>
2023-07-31 19:58:31 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,43 +1,41 @@
 #!/bin/bash
 
-# Check if the script is run with sudo/root privileges
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script requires superuser privileges. Please run with sudo or as root."
-  exit 1
-fi
+# Set variables
+SERVICE_ACCOUNT_KEY='path/to/your/service_account_key.json'
+DOMAIN='aktar.i-leaflets.net'
 
-# Check if the CSV file is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 path/to/users.csv"
-  exit 1
-fi
-
-# CSV file path
-csv_file="$1"
-
-# Function to create a regular user
+# Function to create a user
 function create_user() {
-  local username="$1"
-  local full_name="$2"
-  local job_position="$3"
+  local first_name=$1
+  local last_name=$2
+  local email=$3
+  local employee_title=$4
 
-  # Set a default password or generate a random one
-  local password="Welcome123!"
-  # You can implement a password generation logic if needed
-  # password=$(your_password_generation_function)
+  # Create a JSON payload for the user
+  local payload=$(cat << EOF
+{
+  "name": {
+    "givenName": "$first_name",
+    "familyName": "$last_name"
+  },
+  "primaryEmail": "$email",
+  "password": "password123",  # Set a default password or generate one
+  "changePasswordAtNextLogin": true
+}
+EOF
+)
 
-  # Create the user with home directory and default shell (/bin/bash)
-  useradd -m -c "$full_name, $job_position" -s /bin/bash "$username"
-  echo "$username:$password" | chpasswd
+  # Make the API call to create the user
+  curl --request POST \
+    --header "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+    --header "Content-Type: application/json" \
+    --data "$payload" \
+    "https://www.googleapis.com/admin/directory/v1/users"
 }
 
 # Read the CSV file and create users
-while IFS=, read -r username full_name job_position; do
-  # Skip the header row
-  if [ "$username" != "Username" ]; then
-    create_user "$username" "$full_name" "$job_position"
-    echo "User $username created successfully."
-  fi
-done < "$csv_file"
+while IFS=, read -r first_name last_name email; do
+  create_user "$first_name" "$last_name" "$email" "$employee_title"
+done < 'path/to/your/csv_file.csv'
 
-echo "Regular user creation complete"
+echo "Users created successfully!"
